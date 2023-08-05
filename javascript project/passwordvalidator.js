90ceb030f4c4d0cd33d907b39284aba8999e6ccd
Sabiyafakir <122993785+Sabiyafakir@users.noreply.github.com>
2023-08-05 21:10:25 +0530

    Add files via upload

---
+++ javascript project/passwordvalidator.js
@@ -0,0 +1,4 @@
+let password = "pass@123";
+let cpassword = "pass@124";
+
+password === cpassword ? console.log("Password Matched. Password validation Successful.") : console.log("Password didn't match. Password validation unsuccessful");
